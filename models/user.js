b67d015948dfb634a9e62367ee23b98653c0bc62
yanghua <yanghua1127@gmail.com>
2013-10-11 11:16:26 +0800

    added copyright file

---
+++ models/user.js
@@ -0,0 +1,80 @@
+/*
+  #!/usr/local/bin/node
+  -*- coding:utf-8 -*-
+ 
+  Copyright 2013 yanghua Inc. All Rights Reserved.
+ 
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+ 
+     http://www.apache.org/licenses/LICENSE-2.0
+ 
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  ---
+  Created with Sublime Text 2.
+  User: yanghua
+  Date: 10/10/13
+  Time: 17:30 PM
+  Desc: user - the model of user
+ */
+
+
+/**
+ * model of user
+ */
+var User = function () {
+
+    var model={};
+
+    function getUserId(){
+        return typeof(model.userId)=="undefined" ? "" : model.userId;
+    }
+
+    function getUserName(){
+        return typeof(model.userName)=="undefined" ? "" : model.userName;
+    }
+
+    function getSex(){
+        return typeof(model.sex)=="undefined" ? "" :model.sex;
+    }
+
+    function getDeptId(){
+        return typeof(model.deptId)=="undefined" ? "" : model.deptId;
+    }
+
+    function setUserId(userId){
+        this.model["userId"]=userId;
+    }
+
+    function setUserName(userName){
+        this.model["userName"]=userName;
+    }
+
+    function setSex(sex){
+        this.model["sex"]=sex;
+    }
+
+    function setDeptId(deptId){
+        this.model["deptId"]=deptId;
+    }
+
+    return {
+        getUserId   : getUserId,
+        getUserName : getUserName,
+        getSex      : getSex,
+        getDeptId   : getDeptId,
+        setUserId   : setUserId,
+        setUserName : setUserName,
+        setSex      : setSex,
+        setDeptId   : setDeptId
+    };
+
+}
+
+
+exports.User=User;
