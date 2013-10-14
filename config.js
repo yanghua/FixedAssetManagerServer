2df333d993c207d6bf6ad3b257fcdb8c1a199c6d
yanghua <yanghua1127@gmail.com>
2013-10-14 14:39:52 +0800

    changed config file

---
+++ config.js
@@ -0,0 +1,50 @@
+/*
+  #!/usr/local/bin/node
+  -*- coding:utf-8 -*-
+ 
+  Copyright 2013 yanghua Inc. All Rights Reserved.
+ 
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+ 
+     http://www.apache.org/licenses/LICENSE-2.0
+ 
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  ---
+  Created with Sublime Text 2.
+  User: yanghua
+  Date: 12/10/13
+  Time: 10:00 PM
+  Desc: define config info
+ */
+
+/**
+ * init config info
+ * @return {object} config-info object
+ */
+function initConfig () {
+    var _configInfo = {
+
+      //mysql max connections
+      default_max_conns : 150,
+
+      mysqlConfig       : {
+          "host"      : "172.16.206.16",
+          "port"      : "3306",
+          "user"      : "root",
+          "password"  : "123456"
+      }
+
+    }
+
+    return _configInfo;
+}
+  
+
+//exports
+exports.initConfig=initConfig;
