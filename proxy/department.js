43557e1601ee62c2ee547baec4aa87e06fd7b58b
yanghua <yanghua1127@gmail.com>
2013-11-07 17:42:07 +0800

    released department logic

---
+++ proxy/department.js
@@ -0,0 +1,48 @@
+/*
+  #!/usr/local/bin/node
+  -*- coding:utf-8 -*-
+ 
+  Copyright 2013 yanghua Inc. All Rights Reserved.
+ 
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+ 
+     http://www.apache.org/licenses/LICENSE-2.0
+ 
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  ---
+  Created with Sublime Text 2.
+  User: yanghua
+  Date: 7/11/13
+  Time: 17:25 PM
+  Desc: departments - the controller of departments
+ */
+
+//mode:
+'use strict';
+
+var mysqlUtil    = require("../libs/mysqlUtil"),
+    mysqlClient  = mysqlUtil.initMysql();
+
+/**
+ * get all department
+ * @param  {Function} callback the callback func
+ * @return {null}            
+ */
+exports.getAllDepartment = function (callback) {
+    mysqlClient.query({
+        sql     : "SELECT * FROM DEPARTMENT",
+        params  : {}
+    }, function (err, rows) {
+        if (err || !rows) {
+            return callback(new ServerError(), null);
+        }
+
+        callback(null, rows);
+    });
+};
