92f5307e51e48c19f65e31c5cb580ee8c69a2def
yanghua <yanghua1127@gmail.com>
2013-11-13 18:01:07 +0800

    added a util file for generate guid

---
+++ libs/util.js
@@ -0,0 +1,41 @@
+/*
+  #!/usr/local/bin/node
+  -*- coding:utf-8 -*-
+ 
+  Copyright 2013 yanghua Inc. All Rights Reserved.
+ 
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+ 
+     http://www.apache.org/licenses/LICENSE-2.0
+ 
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  ---
+  Created with Sublime Text 2.
+  User: yanghua
+  Date: 13/11/13
+  Time: 16:49 PM
+  Desc: util - the helper methods
+ */
+
+//mode:
+'use strict';
+
+/**
+ * generate guid
+ */
+exports.GUID = function () {
+    var guid = "";
+    for (var i = 1; i <= 32; i++){
+      var n = Math.floor(Math.random()*16.0).toString(16);
+      guid +=   n;
+      if((i==8)||(i==12)||(i==16)||(i==20))
+        guid += "-";
+    }
+    return guid; 
+};
