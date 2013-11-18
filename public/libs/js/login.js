6a004f9f9b90cc401bad0974fadaf12e51b46347
yanghua <yanghua1127@gmail.com>
2013-11-18 09:52:39 +0800

    fixed captcha img bug
    which cause session lost

--- public/libs/js/login.js
+++ public/libs/js/login.js
@@ -1,9 +1,35 @@
 $(function () {
+    /*
+     *fixed bug: if the document is not ready, 
+     *the request send by src will course session param lost
+     */
+    $("<img src=\"/captchaImg\" class=\"captcha\" />").insertBefore("#refreshBtn");
+
     $(".container .form-signin #refreshBtn").click(function () {
-        $(".container .form-signin .captcha").attr("src","/captchaImg");
+        var jqObj = $(".container .form-signin .captcha");
+        jqObj.prop("src",randomImgSrc(jqObj));
     });
 
     $(".container .form-signin .captcha").click(function () {
-        $(".container .form-signin .captcha").attr("src","/captchaImg");
+        var jqObj = $(".container .form-signin .captcha");
+        jqObj.prop("src",randomImgSrc(jqObj));
     });
 });
+
+/**
+ * get a new url of img's src with a random time
+ * @param  {object} jqObj the img's jquery object
+ * @return {string}       the random src string
+ */
+function randomImgSrc (jqObj) {
+    var src        = jqObj.attr("src");
+    var paramIndex = src.indexOf("?")
+    var hasParam   = paramIndex != -1;
+
+    if (hasParam) {
+        src = src.substring(0, paramIndex);
+    }
+
+    var newSrc = src + "?time=" + new Date().getTime();
+    return newSrc;
+}
