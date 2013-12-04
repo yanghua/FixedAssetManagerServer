a876913e209b497313e1547fa6a8c4631f9a0aa8
ZhichengLiu <xiaoqi55125@gmail.com>
2013-12-04 11:58:09 +0800

    cancel ajax

--- public/libs/js/manage.js
+++ public/libs/js/manage.js
@@ -301,13 +301,13 @@ function searchNoUser (pageIndex) {
 
 function loadAllocToUser (assetId) {
 	alert("..."+assetId);
-	$.ajax({
-		type:"POST",
-		data:{},
-		url:"/fixedasset/"+assetId+"/allocation",
-		success:function (data) {
+	// $.ajax({
+	// 	type:"POST",
+	// 	data:{},
+	// 	url:"/fixedasset/"+assetId+"/allocation",
+	// 	success:function (data) {
 			
-		};
+	// 	};
 
-	})
+	// })
 }
