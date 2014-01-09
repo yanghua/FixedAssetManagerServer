7248379f5a0127b83d714358e2065c07a05f2ed3
Zhicheng Liu <xiaoqi55125@gmail.com>
2014-01-09 10:36:43 +0800

    add import and export func

---
+++ public/libs/js/giftOther.js
@@ -0,0 +1,56 @@
+/**
+ * load ajax upload 
+ * @return {null} 
+ */
+function loadAjaxUpload() {
+	var oBtn = document.getElementById("giftUpload");
+	var oShow = document.getElementById("uploadedName");
+	var oRemind = document.getElementById("errorRemind");
+
+	new AjaxUpload($("#giftUpload"),{
+		action:"/stockin/import",
+		name:"file_source",
+		responseType:"JSON",
+		onSubmit:function(file,ext){
+			if(ext && /^(xls|xlsx)$/.test(ext)){
+				oBtn.value = "正在上传…";
+				//oBtn.disabled = "disabled";
+
+			}else{	
+				oRemind.style.color = "#ff3300";
+				oRemind.innerHTML = "不支持其他格式，请上传Excel文件！";
+				return false;
+			}
+		},
+		onComplete:function(file,response){
+			if (!response.statusCode) {
+				oBtn.value = "上传成功,继续上传?";
+				oRemind.innerHTML = "";
+			}
+		}
+	});
+}
+
+/**
+ * to export excel about stockIn
+ * @return {null} 
+ */
+function exportStockInExcel () {
+	//TO-DO
+}
+
+/**
+ * to export excel about stockOut
+ * @return {null} 
+ */
+function exportStockOutExcel () {
+	//TO-DO
+}
+
+/**
+ * to export excel about stockIn
+ * @return {null} 
+ */
+function exportInventoryExcel () {
+	//TO-DO
+}
