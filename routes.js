655b9406ba040a3f07345c07c3f082cb028f0a6a
Zhicheng Liu <xiaoqi55125@gmail.com>
2014-01-09 14:31:30 +0800

    add stockout export route

--- routes.js
+++ routes.js
@@ -134,6 +134,7 @@ module.exports = function (app) {
     app.post("/stockout/insertion", stockOut.insertion);
     app.post("/stockout/modification", stockOut.modification);
     app.post("/stockout/deletion", stockOut.deletion);
+    app.get("/stockout/export",stockOut.exportSO);
 
     app.get("/limitations", limitation.limitations);
     app.post("/limitation/insertion", limitation.insertion);
